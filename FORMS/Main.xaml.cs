60c23f286cf14a0505ca898a8049b3be793d1179
yingiz95 <dominik.knezovic95@gmail.com>
2020-05-11 23:24:30 +0200

    First Day: Added first GUI with little functions

--- FORMS/Main.xaml.cs
+++ FORMS/Main.xaml.cs
@@ -23,5 +23,60 @@ namespace dtg_admin.FORMS
         {
             InitializeComponent();
         }
+
+        private void Frm_main_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Einfach nur das die Namen in der unteren Combobox stehen
+            Cmbx_user.Items.Add("");
+            Cmbx_user.Items.Add("Yingiz");
+            Cmbx_user.Items.Add("Threnario");
+            Cmbx_user.Items.Add("Tschui");
+            Cmbx_user.Items.Add("Thanatos");
+
+            //Das was in der Search Namensbox ist
+            Cmbx_name.Items.Add("");
+            Cmbx_name.Items.Add("Hey");
+
+
+
+        }
+
+        private void Cmbx_name_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Search aktivieren
+            Btn_search.IsEnabled = true;
+        }
+
+     
+
+        private void Cmbx_name_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //Nach suche alles andere Aktivieren
+            Livi_strikes.IsEnabled = true;
+            Cmbx_user.IsEnabled = true;
+            Txtb_comment.IsEnabled = true;
+            Btn_add.IsEnabled = true;
+            Btn_delete.IsEnabled = true;
+        }
+
+        private void Btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            Livi_strikes.IsEnabled = true;
+            Cmbx_user.IsEnabled = true;
+            Txtb_comment.IsEnabled = true;
+            Btn_add.IsEnabled = true;
+            Btn_delete.IsEnabled = true;
+
+            if (Livi_strikes.Items.Count == 0)
+            {
+                Livi_strikes.Items.Add("Nicht angemeldet");
+                Livi_strikes.Items.Add("Zu Spät");
+                Livi_strikes.Items.Add("Regeln missachtet!(Bemerkung)");
+                Livi_strikes.Items.Add("Member flame");
+                Livi_strikes.Items.Add("Zu schlechte Performance");
+
+            }
+        }
     }
 }
